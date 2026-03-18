a1cfad4c23e51eddc62232061a5f56e2261a963d
MarelGuy <irertnuc.loris@gmail.com>
2026-03-18 09:04:57 +0100

    general optimisations

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+export const SOCIAL_LINKS = {
+    email: "irertnuc.loris@gmail.com",
+    linkedin: "https://www.linkedin.com/in/lc0/",
+    github: "https://github.com/MarelGuy",
+    githubUsername: "MarelGuy",
+    linkedinName: "Loris Cuntreri"
+};
